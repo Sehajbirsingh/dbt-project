d63f6fad4bee6eeab0fbbf2fd30803fbcaa648bc
Sehajbir Singh <Sehajbakshi12@gmail.com>
2024-07-09 08:53:41 +0000

    c

---
+++ macros/custom_schemas.sql
@@ -0,0 +1,9 @@
+-- macros/custom_schemas.sql
+
+{% macro generate_schema_name(custom_schema_name, node) %}
+    {% if custom_schema_name is none %}
+        {{ default_schema }}
+    {% else %}
+        {{ custom_schema_name }}
+    {% endif %}
+{% endmacro %}
